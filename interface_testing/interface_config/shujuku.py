090a7e9a66adea57fc7e2307b22e364d58554011
admin <858042515@qq.com>
2020-07-06 18:36:42 +0800

    已更新导包数据等

---
+++ interface_testing/interface_config/shujuku.py
@@ -0,0 +1,22 @@
+"""[tester]"""
+name = 'zhang'
+
+#[mysplconf]
+host = '127.0.0.1'
+port = '3306'
+user = 'root'
+password = '123456'
+db_name = 'guest'
+
+[user]
+#发送邮箱服务器
+HOST_SERVER = 'smtp.163.com'
+#邮件发送人
+FROM = '858942515@qq.com'
+#邮件收件人
+TO  = '222@123.com'
+#发送邮箱用户名密码
+user = 'aaaa'
+password = 'aaaa'
+#邮件主题
+SUBJECT = '发布会系统接口自动化测试报告'
